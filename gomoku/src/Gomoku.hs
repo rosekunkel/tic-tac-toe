3c7f60207759c86c89abb4d24dd4ac0e2124d87c
nikivazou <nikivazou@gmail.com>
2017-09-20 19:21:27 -0400

    adding timeouts

--- gomoku/src/Gomoku.hs
+++ gomoku/src/Gomoku.hs
@@ -4,9 +4,12 @@ import Types
 import Checks 
 import Misc 
 
-import Player.BestNext    (playerBestNext   )
-import Player.Human       (playerHuman   )
-import Player.Computer    (playerComputer)
+import Player.BestNext    (playerBestNext )
+import Player.Human       (playerHuman    )
+import Player.Computer    (playerComputer )
+
+import Control.Timeout
+import Data.Time.Units (Second)
 
 player1, player2 :: Player
 player1 = playerBestNext
@@ -34,20 +37,25 @@ playRound p1 p2 score i = do
    putStrLn ((if (i `mod` 2 == 0) then show p2 else show p1)  ++ " plays first")
    result <- if (i `mod` 2 == 0) then play p2 p1 emptyBoard else play p1 p2 emptyBoard
    case result of 
-      Just p  -> putStrLn (show p ++ " wins!\n\n") >> return (incr p score)
-      Nothing -> putStrLn "Its a tie!\n\n" >> return score 
+      TimeOut p p' -> putStrLn (show p ++ " timed out after 30sec!\n\n") >> return (incr p' score)
+      Invalid p p' -> putStrLn (show p ++ " made an invalid move!\n\n")  >> return (incr p' score)
+      Wins p       -> putStrLn (show p ++ " wins!\n\n") >> return (incr p score)
+      Tie          -> putStrLn "Its a tie!\n\n" >> return score 
 
 
-play :: PlayerInfo -> PlayerInfo -> Board -> IO (Maybe Winner)
+play :: PlayerInfo -> PlayerInfo -> Board -> IO Result
 play pi1@(PI p1 t1 _) pi2 board = do 
-  move <- (playerMove p1) t1 board
-  case putMaybe board t1 move of
-    Nothing -> putStrLn "Invalid move." >> return (Just pi2)
-    Just b  -> if tileWins b t1
-                  then putStrLn (showWinningBoard b t1) >> return (Just pi1)
-                  else do putStrLn $ showBoardNew move b  
-                          if checkFull b 
-                             then return Nothing 
-                             else play pi2 pi1 b
+  timedMove <- timeout (30::Second) $ (playerMove p1) t1 board
+  case timedMove of 
+    Nothing   -> return $ TimeOut pi1 pi2 
+    Just move -> 
+      case putMaybe board t1 move of
+        Nothing -> putStrLn "Invalid move." >> return (Invalid pi1 pi2)
+        Just b  -> if tileWins b t1
+                      then putStrLn (showWinningBoard b t1) >> return (Wins pi1)
+                      else do putStrLn $ showBoardNew move b  
+                              if checkFull b 
+                                 then return Tie 
+                                 else play pi2 pi1 b
 
                   
