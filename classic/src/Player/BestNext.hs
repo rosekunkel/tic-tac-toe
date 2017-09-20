75f1e90d5e9b7fa46a27d43b7fbf7be803b3b738
nikivazou <nikivazou@gmail.com>
2017-09-20 18:55:51 -0400

    gomoko is ready

--- classic/src/Player/BestNext.hs
+++ classic/src/Player/BestNext.hs
@@ -12,9 +12,9 @@ playerBestNext = Player strategy "Best Next"
 strategy :: Tile -> Board -> IO Move
 strategy tile board = return $ snd $ maximum scoredMoves 
   where
-  	scoredMoves = zip scores moves
-  	scores      = map (evaluateBoard tile . put board tile) moves 
-  	moves       = validMoves board
+    scoredMoves = zip scores moves
+    scores      = map (evaluateBoard tile . put board tile) moves 
+    moves       = validMoves board
 
 
 evaluateBoard :: Tile -> Board -> Int
